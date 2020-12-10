a555f89930292ea3f68c3eb733f27db8ecbe8de3
Blackfire44 <fabi.walli.fw@gmail.com>
2020-12-10 18:14:57 +0100

    Und niemand für einen.

---
+++ 05uebung/build/CMakeFiles/uniform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uniform.dir/uniform.cc.o"
+  "uniform"
+  "uniform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uniform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
