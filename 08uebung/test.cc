7fffbf56603fae3801e4cc99a21a1604bd7a49b6
Blackfire44 <fabi.walli.fw@gmail.com>
2021-01-16 16:39:07 +0100

    jjjjjj

--- 08uebung/test.cc
+++ 08uebung/test.cc
@@ -9,12 +9,12 @@ void mandelbrot (Canvas& canvas, double threshold,int maxIt, std::string filenam
 IterationResult iterate(Point z,Point c, double threshold, int maxIt){
 	int k;
 	//Point r;
-	for(k = 0; k < maxIt && sqrt(z.x()*z.x()+z.y()*z.y())<=threshold; k++){ //Betrag eines vektors ausrechene!!!!
+	for(k = 0; k < maxIt && sqrt(z.x()*z.x()+z.y()*z.y())<=threshold; k++){
 		//Point z(z.x()*z.x()-z.y()*z.y()+c.x(), 2*z.x()*z.y()+c.y());
 		z = Point(z.x()+1, z.y()+1);
 	}
 	if(k == 0){
-		Point z(0, 0);
+		z = Point(0, 0);
 	}
 	IterationResult i(z, k);
 	return(i);
@@ -22,7 +22,7 @@ IterationResult iterate(Point z,Point c, double threshold, int maxIt){
 
 int main(int argc, char *argv[]){
 	Point p(4,2);
-	Point z(1,1);
+	Point z(0,0);
 	Point u(0,0);
 	IterationResult i = iterate(z, u,10, 10);
 	std::cout << i.getPoint().x() << "  " << i.getPoint().y() << std::endl;
