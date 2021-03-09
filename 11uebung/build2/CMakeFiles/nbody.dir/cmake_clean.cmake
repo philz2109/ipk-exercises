9ed1c8210837bd26ef4cae2b3d3ff0341e2f6ab5
philz2109 <74725188+philz2109@users.noreply.github.com>
2021-03-09 13:49:57 +0100

    j

---
+++ 11uebung/build2/CMakeFiles/nbody.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nbody.dir/io.cc.o"
+  "CMakeFiles/nbody.dir/nbody.cc.o"
+  "nbody"
+  "nbody.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nbody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
